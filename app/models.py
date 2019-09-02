ed21dc646d25a57cb6a7fb9ab21dfc245bf7273f
James Kirwa <example@email.com>
2019-09-02 11:28:37 +0300

    add comment to models.py

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-# Create your models here.
+# Create your own models here.
 class Profile(models.Model):
     photo = models.ImageField(upload_to = 'media/')
     bio = models.TextField(default="Hey there!I\'m using Instagram")
